b000840b38c74e0710c7be98bd9a9fb6a28c26ef
dm4rt <danmartinez2004@gmail.com>
2024-05-17 16:15:03 -0400

    added naviaget to Single camps view

--- src/components/views/StudentView.js
+++ src/components/views/StudentView.js
@@ -4,6 +4,9 @@ StudentView.js
 The Views component is responsible for rendering web page with data provided by the corresponding Container component.
 It constructs a React component to display the single student view page.
 ================================================== */
+import React from 'react';
+import { Link } from 'react-router-dom';
+
 const StudentView = (props) => {
   const { student } = props;
 
@@ -11,7 +14,11 @@ const StudentView = (props) => {
     <div>
       <h1>{student.firstname + " " + student.lastname}</h1>
       {student.campus ? (
-        <h3>{student.campus.name}</h3>
+        <h3>
+          <Link to={`/campus/${student.campus.id}`}>
+            {student.campus.name}
+          </Link>
+        </h3>
       ) : (
         <h3>No campus assigned</h3>
       )}
@@ -20,3 +27,5 @@ const StudentView = (props) => {
 };
 
 export default StudentView;
+
+
